ca07f77c9c0e1c917072a4dc2fd714e8ffb3d545
satishvatturu <93177666+satishvatturu@users.noreply.github.com>
2023-11-02 19:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById("firstName").value;
+  	const lastName = document.getElementById("lastName").value;
 
+  if (firstName && lastName) {
+    alert( firstName +" " + lastName);
+  } else {
+    alert("Please fill out both First Name and Last Name fields.");
+  }
 }
